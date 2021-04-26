a5482dd1e19fe0bd3fab30f5e740856cbb76463f
Salvatore Tidu <salvatoret9@outlook.it>
2021-04-26 20:13:19 +0200

    PHP_Google_FAQ_(Aggiornamento)

--- index.php
+++ index.php
@@ -47,10 +47,10 @@
 
                 "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:
 
-                1. La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
-                    a. Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.
-                    b. Google LLC, con sede negli Stati Uniti, per il resto del mondo.
-                2. La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.",
+                <ol class='top_ol'><li>1. La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
+                    <ol class='bottom_ol'><li>a. Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</li>
+                    <li>b. Google LLC, con sede negli Stati Uniti, per il resto del mondo.</li></ol>
+                <li>2. La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.</li></ol>",
 
                 "Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.",
 
